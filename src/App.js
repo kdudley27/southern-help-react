a009c8498908e339d7dffdcb9451f7a8ddb40cc7
Kevin Dudley <kevin.dudley27@gmail.com>
2018-09-18 12:48:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from './componets/Layout'
+import './bootstrap.css'
+import './ie8.css'
+import './ie9.css'
+
+class App extends Component {
+  render() {
+    return (
+     <Layout />
+    );
+  }
+}
+
+export default App;
